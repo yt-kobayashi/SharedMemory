e2880921cab20a9719e2b3a21a3bd75aec6a4d77
yt-kobayashi <270yoshikoba@gmail.com>
2021-02-25 06:10:51 +0900

    [UPDATE] Added private variables.

--- SharedMemory/SharedMemory/SharedMemory.h
+++ SharedMemory/SharedMemory/SharedMemory.h
@@ -7,5 +7,9 @@
 
 ref class SharedMemory
 {
+private:
+	HANDLE mappingFileHandle;					// Handle of mapping file.
+	HANDLE mappingFileMutexHandle = NULL;		// Mutex object for exclusive access to the mapping file.
+	bool isExists;								// Created flag. (True : Created,	False : Not created)
 };
 
