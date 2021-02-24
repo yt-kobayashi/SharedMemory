e901ba81e0e1a9bbc1c18cf330d14c7da87deefb
yt-kobayashi <270yoshikoba@gmail.com>
2021-02-25 06:22:24 +0900

    [UPDATE] Added const variables.
    Added names of mapping file and mutex object.

--- SharedMemory/SharedMemory/SharedMemory.h
+++ SharedMemory/SharedMemory/SharedMemory.h
@@ -8,8 +8,13 @@
 ref class SharedMemory
 {
 private:
+	// Variable
 	HANDLE mappingFileHandle;					// Handle of mapping file.
 	HANDLE mappingFileMutexHandle = NULL;		// Mutex object for exclusive access to the mapping file.
 	bool isExists;								// Created flag. (True : Created,	False : Not created)
+
+	// Const Variable
+	const char* mappingFileName = "TestMappringFileName";	// Name of the mapping file.
+	const char* mutexObjectName = "TestMutexObjectName";	// Name of mutex object.
 };
 
